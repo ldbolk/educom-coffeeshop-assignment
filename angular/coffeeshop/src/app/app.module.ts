22393509868614b1b1f712e50dd09fef8b799594
ldbolk <ldbolk@ziggo.nl>
2022-07-15 11:39:32 +0200

    Didn't break yet

--- angular/coffeeshop/src/app/app.module.ts
+++ angular/coffeeshop/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatExpansionModule } from '@angular/material/expansion'
 import { MatSelectModule } from '@angular/material/select'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
